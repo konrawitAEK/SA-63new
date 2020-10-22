e26c8f40d119b05a1f88a9ce054a0000b0e0701e
konrawitAEK <aekpeerat2542@gmail.com>
2020-10-22 23:30:50 +0700

    update

--- frontend/plugins/welcome/src/plugin.ts
+++ frontend/plugins/welcome/src/plugin.ts
@@ -1,13 +1,13 @@
 import { createPlugin } from '@backstage/core';
 import WelcomePage from './components/WelcomePage';
-import CreateUser from './components/Users';
+import adddata from './components/Adddata';
 import main from './components/Main';
 
 export const plugin = createPlugin({
   id: 'welcome',
   register({ router }) {
     router.registerRoute('/', WelcomePage);
-    router.registerRoute('/user', CreateUser);
+    router.registerRoute('/user', adddata);
     router.registerRoute('/main', main);
   },
 });
